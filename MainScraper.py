ff97fc1a719157c1543d90a0ee1aec6ad175ec58
dylandru <158249525+dylandru@users.noreply.github.com>
2024-08-30 16:13:45 -0400

    Returns DF containing Play Information
    Needs to return df so that can user can access information about the Play IDs (such as pitcher, hitter, game situation) that were scraped.

--- MainScraper.py
+++ MainScraper.py
@@ -14,7 +14,7 @@ def run_statcast_pull_scraper(start_date: str,
                               max_workers: int = 5, 
                               team: str = None, 
                               pitch_call: str = None,
-                              max_videos: int = None) -> None:
+                              max_videos: int = None) -> pd.DataFrame:
     """
     Run scraper from Statcast Pull of Play IDs. Retrieves data and processes each row in parallel.
 
@@ -28,34 +28,37 @@ def run_statcast_pull_scraper(start_date: str,
         max_videos (int, optional): Max number of videos to pull. Defaults to None.
 
     Returns:
-        None: A collection of Baseball Savant videos downloaded to a specified directory.
+        pd.DataFrame: DataFrame containing Play IDs and relavent information on the videos that were scraped.
 
     Raises:
         Exception: Any error in downloading a video for a given play. 
     
     """
+    try:
+        session = requests.Session()
+        df = playids_for_date_range(start_date=start_date, end_date=end_date, team=team, pitch_call=pitch_call) #retrieves Play IDs to scrape
 
-    session = requests.Session()
-    df = playids_for_date_range(start_date=start_date, end_date=end_date, team=team, pitch_call=pitch_call) #retrieves Play IDs to scrape
+        if not df.empty and 'play_id' in df.columns:
+            os.makedirs(download_folder, exist_ok=True)
 
-    if not df.empty and 'play_id' in df.columns:
-        os.makedirs(download_folder, exist_ok=True)
+            if max_videos is not None:
+                df = df.head(max_videos) #limits length of Play ID df to the amount of max videos if specified
 
-        if max_videos is not None:
-            df = df.head(max_videos) #limits length of Play ID df to the amount of max videos if specified
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                future_to_play_id = {executor.submit(get_video_for_play_id, row['play_id'], row['game_pk'], download_folder): row for _, row in df.iterrows()} #sets futures to download videos for given Play IDs 
+                for future in as_completed(future_to_play_id):
+                    play_id = future_to_play_id[future]
+                    try:
+                        future.result() #get result from future tasks
+                    except Exception as e:
+                        print(f"Error processing Play ID {play_id['play_id']}: {str(e)}")
+        else:
+            print("Play ID column not in Statcast pull or DataFrame is empty")
 
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_to_play_id = {executor.submit(get_video_for_play_id, row['play_id'], row['game_pk'], download_folder): row for _, row in df.iterrows()} #sets futures to download videos for given Play IDs 
-            for future in as_completed(future_to_play_id):
-                play_id = future_to_play_id[future]
-                try:
-                    future.result() #get result from future tasks
-                except Exception as e:
-                    print(f"Error processing Play ID {play_id['play_id']}: {str(e)}")
-    else:
-        print("Play ID column not in Statcast pull or DataFrame is empty")
+    except KeyboardInterrupt:
+        print("Ctrl+C detected. Shutting down.")
 
-    return None
+    return df
 
 def run_csv_pull_scraper(reference_sheet: str, 
                          download_folder: str, 
@@ -96,7 +99,7 @@ def run_csv_pull_scraper(reference_sheet: str,
 """ EXAMPLE CALL """
         
 if __name__ == "__main__":
-    download_folder = "/Users/dylandrummey/Downloads/DylanDru_GitHub/Baseball-Savant-Video-Scraper/savant_video_utils/test1/"
+    download_folder = "./test1/"
     run_statcast_pull_scraper(start_date="2023-04-12", end_date="2023-04-12", download_folder=download_folder)
 
 	
